ed69bd4e5a11f92e4cb440ec679fa021ad93f938
Moh <https://github.com/EngMohammed23/damage_assessment_project.git>
2024-10-31 15:29:39 -0700

    update commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:fluid_bottom_nav_bar/fluid_bottom_nav_bar.dart';
 
 void main() {
   runApp(const MainApp());
@@ -22,11 +23,35 @@ class MainApp extends StatelessWidget {
       ],
       locale: const Locale('ar', ''), // لتعيين العربية كافتراضية للتجربة
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: FluidNavBarDemo(),
     );
   }
 }
 
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('مرحبا بك'), // "مرحبا بك" بالعربية
+//       ),
+//       backgroundColor: Color(0xFF75B7E1),
+//       extendBody: true,
+//       body: Center(
+//         child: Text(
+//             'هذا تطبيق يدعم اللغة العربية.'), // "هذا تطبيق يدعم اللغة العربية."
+//       ),
+//       bottomNavigationBar: FluidNavBar(                     // (1)
+//           icons: [                                            // (2)
+//             FluidNavBarIcon(svgPath: "assets/home.svg"),      // (3)
+//             FluidNavBarIcon(svgPath: "assets/bookmark.svg"),
+//           ],
+//           onChange: _handleNavigationChange,                  // (4)
+//         ),
+
+//     );
+//   }
+// }
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -34,10 +59,91 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('مرحبا بك'), // "مرحبا بك" بالعربية
       ),
+      backgroundColor: Color(0xFF75B7E1),
+      extendBody: true,
       body: Center(
         child: Text(
             'هذا تطبيق يدعم اللغة العربية.'), // "هذا تطبيق يدعم اللغة العربية."
       ),
     );
+  }
+}
+
+class FluidNavBarDemo extends StatefulWidget {
+  @override
+  State createState() {
+    return _FluidNavBarDemoState();
+  }
+}
+
+class _FluidNavBarDemoState extends State {
+  Widget? _child;
+
+  @override
+  void initState() {
+    _child = HomePage();
+    super.initState();
+  }
+
+  @override
+  Widget build(context) {
+    // Build a simple container that switches content based of off the selected navigation item
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFF75B7E1),
+        extendBody: true,
+        body: _child,
+        bottomNavigationBar: FluidNavBar(
+          icons: [
+            FluidNavBarIcon(
+                svgPath: "assets/home.svg",
+                backgroundColor: Color(0xFF4285F4),
+                extras: {"label": "home"}),
+            FluidNavBarIcon(
+                icon: Icons.bookmark_border,
+                backgroundColor: Color(0xFFEC4134),
+                extras: {"label": "bookmark"}),
+            FluidNavBarIcon(
+                icon: Icons.apps,
+                backgroundColor: Color(0xFFFCBA02),
+                extras: {"label": "partner"}),
+            FluidNavBarIcon(
+                svgPath: "assets/conference.svg",
+                backgroundColor: Color(0xFF34A950),
+                extras: {"label": "conference"}),
+          ],
+          onChange: _handleNavigationChange,
+          style: FluidNavBarStyle(iconUnselectedForegroundColor: Colors.white),
+          scaleFactor: 1.5,
+          defaultIndex: 1,
+          itemBuilder: (icon, item) => Semantics(
+            label: icon.extras!["label"],
+            child: item,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _handleNavigationChange(int index) {
+    setState(() {
+      switch (index) {
+        case 0:
+          _child = HomePage();
+          break;
+        case 1:
+          _child = HomePage();
+          break;
+        case 2:
+          _child = HomePage();
+          break;
+      }
+      _child = AnimatedSwitcher(
+        switchInCurve: Curves.easeOut,
+        switchOutCurve: Curves.easeIn,
+        duration: Duration(milliseconds: 500),
+        child: _child,
+      );
+    });
   }
 }
