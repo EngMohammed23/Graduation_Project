cbc718fe8db6015b971bc9c27b52122d90485810
unknown <mdmeg1010@gmail.com>
2024-12-05 15:45:13 +0200

    latest upgrade

--- lib/views/pageview2.dart
+++ lib/views/pageview2.dart
@@ -1,5 +1,55 @@
 import 'package:flutter/material.dart';
 
+// class Pageview2 extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Container(
+//             width: 350,
+//             height: 450,
+//             margin: EdgeInsets.only(top: 20),
+//             decoration: BoxDecoration(
+//               image: DecorationImage(
+//                 image: AssetImage('assets/images/two.jpg'),
+//                 fit: BoxFit.cover,
+//               ),
+//               border: Border.all(color: Colors.grey),
+//               borderRadius: BorderRadius.all(Radius.circular(15.0)),
+//             ),
+//           ),
+//           Container(
+//             margin: EdgeInsets.only(top: 40),
+//             child: Text("Connect with certified contractors"),
+//           ),
+//           SizedBox(height: 20),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//             children: [
+//               ElevatedButton(
+//                 onPressed: () {
+//                   // Add your functionality here
+//                   print("Button 1 clicked!");
+//                 },
+//                 child: Text("Button 1"),
+//               ),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   // Add your functionality here
+//                   print("Button 2 clicked!");
+//                 },
+//                 child: Text("Button 2"),
+//               ),
+//             ],
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
 class Pageview2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
--- lib/views/welcome_screen.dart
+++ lib/views/welcome_screen.dart
@@ -6,6 +6,78 @@ import 'package:damage_assessment_project/views/pageview3.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+// class WelcomeScreen extends StatelessWidget {
+//   final WelcomeController controller = Get.put(WelcomeController());
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Column(
+//         children: [
+//           // "Skip" button
+//           Container(
+//             margin: EdgeInsets.fromLTRB(0, 40, 10, 0),
+//             alignment: Alignment.centerRight,
+//             child: TextButton(
+//                 onPressed: () {
+//                   Get.to(LoginScreen());
+//                 },
+//                 child: Text("Skip")),
+//           ),
+
+//           // PageView for sliding pages
+//           Expanded(
+//             child: PageView(
+//               controller: controller.pageController,
+//               onPageChanged: controller.onPageChanged,
+//               children: [
+//                 Pageview1(),
+//                 Pageview2(),
+//                 Pageview3(),
+//               ],
+//             ),
+//           ),
+
+//           // Page indicators
+//           Obx(() => Row(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: List.generate(3, (index) {
+//                   return Container(
+//                     margin: EdgeInsets.symmetric(horizontal: 4),
+//                     width: controller.currentPage.value == index ? 20 : 8,
+//                     height: 8,
+//                     decoration: BoxDecoration(
+//                       color: controller.currentPage.value == index
+//                           ? Colors.blue
+//                           : Colors.grey,
+//                       borderRadius: BorderRadius.circular(4),
+//                     ),
+//                   );
+//                 }),
+//               )),
+
+//           // Conditionally render the button for PageView1
+//           Obx(() {
+//             if (controller.currentPage.value == 0) {
+//               return Padding(
+//                 padding: const EdgeInsets.only(top: 16.0),
+//                 child: IconButton(
+//                     onPressed: () {}, icon: Icon(Icons.arrow_forward_rounded)),
+//               );
+//             }
+//             return SizedBox.shrink(); // Hide the button for other pages
+//           }),
+
+//           // Bottom padding to prevent content overflow
+//           Padding(
+//             padding: const EdgeInsets.all(20.0),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
 class WelcomeScreen extends StatelessWidget {
   final WelcomeController controller = Get.put(WelcomeController());
 
@@ -14,15 +86,19 @@ class WelcomeScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
+          // "Skip" button
           Container(
             margin: EdgeInsets.fromLTRB(0, 40, 10, 0),
             alignment: Alignment.centerRight,
             child: TextButton(
-                onPressed: () {
-                  Get.to(LoginScreen());
-                },
-                child: Text("Skip")),
+              onPressed: () {
+                Get.to(LoginScreen());
+              },
+              child: Text("Skip"),
+            ),
           ),
+
+          // PageView for sliding pages
           Expanded(
             child: PageView(
               controller: controller.pageController,
@@ -34,6 +110,8 @@ class WelcomeScreen extends StatelessWidget {
               ],
             ),
           ),
+
+          // Page indicators
           Obx(() => Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: List.generate(3, (index) {
@@ -50,8 +128,61 @@ class WelcomeScreen extends StatelessWidget {
                   );
                 }),
               )),
+
+          // Conditionally render buttons under the indicator
+          Obx(() {
+            if (controller.currentPage.value == 0) {
+              // Button for PageView1
+              return Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.arrow_forward_rounded),
+                ),
+              );
+            } else if (controller.currentPage.value == 1) {
+              // Two buttons in a row for PageView2
+              return Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        // Add functionality for Button 1
+                        print("Button 1 clicked on PageView2!");
+                      },
+                      child: Text("Button 1"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Add functionality for Button 2
+                        print("Button 2 clicked on PageView2!");
+                      },
+                      child: Text("Button 2"),
+                    ),
+                  ],
+                ),
+              );
+            } else if (controller.currentPage.value == 2) {
+              // Single button for PageView3
+              return Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Add functionality for the button
+                    print("Button clicked on PageView3!");
+                  },
+                  child: Text("Pageview3 Button"),
+                ),
+              );
+            }
+            return SizedBox.shrink(); // Hide if no condition is met
+          }),
+
+          // Bottom padding to prevent content overflow
           Padding(
-            padding: const EdgeInsets.all(100.0),
+            padding: const EdgeInsets.all(20.0),
           ),
         ],
       ),
