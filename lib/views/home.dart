54dc198c41fbba79c0d10a763e1389823b256a99
hesham <hesham.7.r@gmail.com>
2024-12-15 09:30:38 +0200

    الهوم وتباعاته

---
+++ lib/views/home.dart
@@ -0,0 +1,227 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Home extends StatelessWidget {
+  final List<String> _projectImages = [
+    'assets/images/three.jpg',
+    'assets/images/three.jpg',
+    'assets/images/three.jpg',
+    'assets/images/three.jpg',
+  ];
+  final List<String> _projectNames = [
+    'Sound of Sky',
+    'Girl on Fire',
+    'Sound of Sky',
+    'Girl on Fire',
+  ];
+  final List<String> _projectDescriptions = [
+    'Dilon Bruce',
+    'Alecia Keys',
+    'Dilon Bruce',
+    'Alecia Keys',
+  ];
+  final List<String> _projectPrices = [
+    '\$39',
+    '\$50',
+    '\$90',
+    '\$80',
+  ];
+
+  final List<String> _contractorImages = [
+    'assets/images/three.jpg',
+    'assets/images/three.jpg',
+    'assets/images/three.jpg',
+  ];
+  final List<String> _contractorNames = [
+    'Sound of Sky',
+    'Girl on Fire',
+    'Sound of Sky',
+  ];
+  final List<String> _contractorDescriptions = [
+    'Dilon Bruce',
+    'Alecia Keys',
+    'Dilon Bruce',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: ListView(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(6),
+              child: Image.asset(
+                'assets/images/three.jpg',
+                width: double.infinity,
+                height: 182,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Projects',
+                  style: GoogleFonts.poppins(
+                    color: Colors.black,
+                    fontSize: 22,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'See all',
+                    style: GoogleFonts.poppins(
+                      color: const Color(0xFF979797),
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            SizedBox(
+              height: 250,
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemCount: _projectImages.length,
+                separatorBuilder: (context, index) => const SizedBox(width: 13),
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 200,
+                    padding: const EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(
+                        width: 1,
+                        color: const Color(0xFFCDD4D9),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(6),
+                          child: Image.asset(
+                            _projectImages[index],
+                            width: double.infinity,
+                            height: 120,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          _projectNames[index],
+                          style: GoogleFonts.poppins(
+                            color: Colors.black,
+                            fontSize: 15,
+                          ),
+                        ),
+                        const SizedBox(height: 2),
+                        Text(
+                          _projectDescriptions[index],
+                          style: GoogleFonts.poppins(
+                            color: const Color(0xFF979797),
+                            fontSize: 12,
+                          ),
+                        ),
+                        const SizedBox(height: 3),
+                        Text(
+                          _projectPrices[index],
+                          style: GoogleFonts.poppins(
+                            color: Colors.black,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Contractors',
+                  style: GoogleFonts.poppins(
+                    color: Colors.black,
+                    fontSize: 22,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'See all',
+                    style: GoogleFonts.poppins(
+                      color: const Color(0xFF979797),
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            SizedBox(
+              height: 250,
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemCount: _contractorImages.length,
+                separatorBuilder: (context, index) => const SizedBox(width: 13),
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 200,
+                    padding: const EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      border: Border.all(
+                        width: 1,
+                        color: const Color(0xFFCDD4D9),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Center(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.asset(
+                              _contractorImages[index],
+                              width: 120,
+                              height: 140,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          _contractorNames[index],
+                          style: GoogleFonts.poppins(
+                            color: Colors.black,
+                            fontSize: 13,
+                          ),
+                        ),
+                        const SizedBox(height: 2),
+                        Text(
+                          _contractorDescriptions[index],
+                          style: GoogleFonts.poppins(
+                            color: const Color(0xFF636363),
+                            fontSize: 10,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/home_screen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:takatuf/views/profile_screen.dart';
+import '../widget/home_card_widget.dart';
+import '../widget/list_tile_drawer.dart';
+import 'Home.dart';
+import 'add_screen.dart';
+import 'favorites_screen.dart';
+import 'search_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  final List<Widget>_list =[
+    Home(),
+    SearchScreen(),
+    AddScreen(),
+    FavoritesScreen(),
+    ProfileScreen()
+  ];
+  final List<Widget>_list2 =[
+    // Container(
+    //   height: 37,
+    //   width: 281,
+    //   child: TextField(
+    //       style: TextStyle(color: Colors.white),
+    //       decoration: InputDecoration(
+    //         hintText: 'Search album song ',
+    //         hintStyle: TextStyle(
+    //           color: Color(0XFF707070),
+    //           fontSize: 13,
+    //           fontWeight: FontWeight.normal,
+    //         ),
+    //         prefixIcon: Icon(Icons.search,color: Color(0XFF707070)),
+    //         border: OutlineInputBorder(
+    //             borderRadius: BorderRadius.circular(25),
+    //             borderSide: BorderSide(color: Colors.grey.shade700)),
+    //       )),
+    // ),
+    Text('Home'),
+    Text('Search'),
+    Text('AddScreen'),
+    Text('FavoritesScreen'),
+    Text('ProfileScreen'),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        drawer: Drawer(
+          child: Column(
+
+          )
+        ),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          // leading: ,
+          title: _list2.elementAt(_currentIndex),
+          centerTitle:true ,
+          actions: [
+            Row(
+              children: [
+                IconButton(onPressed: () {
+
+                },icon: Icon(Icons.settings),),
+                IconButton(onPressed: () {
+
+                },icon: Icon(Icons.favorite),)
+              ],
+            )
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (value){
+            setState((){
+              _currentIndex =value;
+            });
+          },
+          backgroundColor: const Color(0XFF003366),
+          unselectedItemColor: const Color(0XFF63666E),
+          fixedColor:  Colors.white,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home,), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.search,), label: 'Search'),
+            BottomNavigationBarItem(icon: Icon(Icons.add_circle_outline,), label: 'Add'),
+            BottomNavigationBarItem(icon: Icon(Icons.favorite_border,), label: 'Favorite'),
+            BottomNavigationBarItem(icon: Icon(Icons.person,), label: 'Person'),
+          ],
+        ),
+        body:
+        _list.elementAt(_currentIndex)
+
+    );
+  }
+}
