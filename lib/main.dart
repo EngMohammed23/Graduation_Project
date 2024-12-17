80e48673d342b29fe2eb3077476da2bff227cfb0
Ahmad Khaled Shwadeh <ahmad.11.5.2001@gmail.com>
2024-12-17 18:52:17 +0200

    make success screen and name , logo app

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:takatuf/views/home_screen.dart';
-import 'package:takatuf/views/splash.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+class SuccessScreen extends StatelessWidget {
+  final VoidCallback onSuccess;
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  SuccessScreen({required this.onSuccess});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+    // إغلاق الواجهة بعد ثانيتين
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.of(context).pop(); // إغلاق الشاشة
+      onSuccess(); // تنفيذ الإجراء بعد الإغلاق
+    });
+
+    return Container(
+      margin: EdgeInsets.only(top: 100), // مسافة من الأعلى لتظهر AppBar
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(16),
+          topRight: Radius.circular(16),
+        ),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 150,
+            height: 150,
+            decoration: BoxDecoration(
+              color: Colors.blue.shade100,
+              shape: BoxShape.circle,
+            ),
+            child: Icon(
+              Icons.check,
+              size: 80,
+              color: Color(0xFF003366),
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Success!',
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Poppins',
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
